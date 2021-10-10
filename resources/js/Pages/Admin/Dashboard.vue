48078d420586d26c2b45ea6b158cad04cd43f2d3
Noman Abdullah <71120198+nomanabdullah-dev@users.noreply.github.com>
2021-10-11 00:11:40 +0600

    adminlayout template done

---
+++ resources/js/Pages/Admin/Dashboard.vue
@@ -0,0 +1,28 @@
+<template>
+    <admin-layout>
+        <template #header>
+            <h1 class="font-semibold text-xl text-gray-800 leading-tight">
+                Admin Dashboard
+            </h1>
+        </template>
+
+        <div class="px-6 py-2">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, iure aliquid molestias aperiam autem corrupti facilis repudiandae asperiores voluptatibus magni alias, molestiae dolorum assumenda laborum necessitatibus fuga tenetur ipsa provident.
+        </div>
+    </admin-layout>
+</template>
+
+<script>
+    import Welcome from '@/Pages/Welcome'
+    import AdminLayout from '@/Layouts/AdminLayout'
+    export default {
+        components: {
+            AdminLayout,
+            Welcome
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,10 @@
 <?php
 
+use App\Http\Controllers\Admins\AdminDashboardController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
@@ -27,3 +18,7 @@ Route::get('/', function () {
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->name('dashboard');
+
+Route::prefix('admin')->middleware(['auth:sanctum','verified'])->name('admin.')->group(function() {
+    Route::get('dashboard', [AdminDashboardController::class,'index'])->name('dashboard');
+});
