d1342235545a5f145c6b2d80756923290147fa2c
Noman Abdullah <71120198+nomanabdullah-dev@users.noreply.github.com>
2021-10-17 01:44:47 +0600

    separate route define for user and admin

---
+++ app/Http/Responses/LoginResponse.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Responses;
+
+use Laravel\Fortify\Contracts\LoginResponse as LoginResponseContract;
+use Laravel\Fortify\Fortify;
+
+class LoginResponse implements LoginResponseContract
+{
+    /**
+     * Create an HTTP response that represents the object.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function toResponse($request)
+    {
+        $role = auth()->user()->is_admin;
+        if($request->wantsJson()) {
+            return response()->json(['two_factor' => false]);
+        }
+        switch ($role) {
+            case '0':
+                return redirect()->intended(config('fortify.home'));
+                break;
+            case '1':
+                return redirect()->route('admin.dashboard');
+                break;
+            default:
+                return redirect('/');
+        }
+        // return $request->wantsJson()
+        //             ? response()->json(['two_factor' => false])
+        //             : redirect()->intended(Fortify::redirects('login'));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,13 @@
 <?php
 
-use App\Http\Controllers\Admin\UserController;
-use App\Http\Controllers\Admins\AdminController;
-use App\Http\Controllers\Admins\AdminDashboardController;
-use App\Http\Controllers\Admins\RoleController;
-use Illuminate\Foundation\Application;
-use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Application;
+use App\Http\Controllers\Admin\UserController;
+use App\Http\Controllers\Admins\RoleController;
+use App\Http\Controllers\Admins\AdminController;
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Admins\AdminDashboardController;
 
 
 Route::get('/', function () {
@@ -28,10 +29,18 @@ Route::prefix('admin')->middleware(['auth:sanctum','verified'])->name('admin.')-
     Route::resource('roles', RoleController::class)->except(['edit']);
     Route::resource('admins', AdminController::class)->parameters(['admins' => 'user'])->only(['index', 'show', 'update']);
     Route::resource('users', UserController::class)->parameters(['users' => 'user'])->only(['index', 'show', 'update']);
+});
 
-    // Route::prefix('users')->name('users.')->group(function() {
-    //     Route::get('/', [UserController::class, 'index'])->name('index');
-    //     Route::get('/{user}', [UserController::class, 'show'])->name('show');
-    //     Route::patch('/{user}', [UserController::class, 'update'])->name('update');
-    // });
+
+Route::group(['middleware' => config('fortify.middleware', ['web'])], function () {
+    $limiter = config('fortify.limiters.login');
+
+    Route::post('/login', [AuthenticatedSessionController::class, 'store'])
+        ->middleware(array_filter([
+            'guest:'.config('fortify.guard'),
+            $limiter ? 'throttle:'.$limiter : null,
+        ]));
+
+    Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
+        ->name('logout');
 });
